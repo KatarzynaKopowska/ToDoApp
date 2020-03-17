be0674d9c1d2f0acf338f9f5b93c3d83f44b77f3
Katarzyna <katarzyna.kopowska@gmail.com>
2020-03-17 10:53:00 +0100

    Create Task model

--- ToDoApp/ToDoCard/models.py
+++ ToDoApp/ToDoCard/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    due_date = models.DateTimeField()
+    is_finished = models.BooleanField()
+
