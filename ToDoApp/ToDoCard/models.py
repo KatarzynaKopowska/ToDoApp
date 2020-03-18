606669f7f2939089efcd7605ed7c919c5b9dffac
Katarzyna <katarzyna.kopowska@gmail.com>
2020-03-18 11:09:59 +0100

    Add administration

--- ToDoApp/ToDoCard/models.py
+++ ToDoApp/ToDoCard/models.py
@@ -5,11 +5,15 @@ from django.utils import timezone
 
 
 class Task(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
-    due_date = models.DateTimeField()
+    due_date = models.DateTimeField(blank=True, null=True)
     is_finished = models.BooleanField()
+
+    def __str__(self):
+        return self.author
 
 
 class Subtask(models.Model):
