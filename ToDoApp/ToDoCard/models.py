5e33abed6f7bccdc39338f2f2d621de20a96cd58
Katarzyna <katarzyna.kopowska@gmail.com>
2020-04-04 10:49:56 +0200

    Update Task model in models.py

--- ToDoApp/ToDoCard/models.py
+++ ToDoApp/ToDoCard/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 
 
 class Task(models.Model):
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    author = models.CharField(max_length=200)
     title = models.CharField(max_length=200)
     description = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
